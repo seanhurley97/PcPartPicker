25702db075ac8cd997ce0b6f404914111d8eb9f4
seanhurley97 <student@shurley.local>
2015-04-22 11:44:51 -0500

    initial commit

---
+++ PcPartPicker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PcPartPicker
+//
+//  Created by Sean Hurley on 4/20/15.
+//  Copyright (c) 2015 Sean Hurley. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let testObject = PFObject(className: "TestObject")
+        testObject["foo"] = "bar"
+        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+        println("Object has been saved.")}
+    }
+}
